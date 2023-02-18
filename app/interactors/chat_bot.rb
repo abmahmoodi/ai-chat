f7891aebd92506f00eeacef4fcfa39a7bf9ef9e1
Abolfazl Mahmoudi <msd.soft@gmail.com>
2023-02-18 13:46:15 +0330

    error handling, added reply_to_message_id param

--- app/interactors/chat_bot.rb
+++ app/interactors/chat_bot.rb
@@ -3,6 +3,8 @@ class ChatBot
   include Interactor
 
   TOKEN = ENV['TELEGRAM_TOKEN']
+  PARAM_ERROR = 'Please ask your question with /ai like /ai what is AI?'
+
   def call
     Telegram::Bot::Client.run(TOKEN) do |bot|
       bot.logger.info('Bot has been started')
@@ -16,7 +18,11 @@ class ChatBot
         elsif message.text[0..2] == '/ai'
           query = message.text[3..-1]
           answer = Chat.call(message: query).result
-          bot.api.send_message(chat_id: message.chat.id, text: answer)
+          bot.api.send_message(chat_id: message.chat.id,
+                               reply_to_message_id: message.id,
+                               text: answer)
+        else
+          bot.api.send_message(chat_id: message.chat.id, text: PARAM_ERROR)
         end
         rescue StandardError => e
           Rails.logger.info(e.message)
