55066822aa82b2e666389cf640d14803616610d9
Abolfazl Mahmoudi <msd.soft@gmail.com>
2023-02-25 12:51:03 +0330

    deprecated ai command.

--- app/interactors/chat_bot.rb
+++ app/interactors/chat_bot.rb
@@ -6,6 +6,7 @@ class ChatBot
   PARAM_ERROR = "Please ask your question from AI like: \n/ai what is AI?"
   HELP = "Ask question: /ai\nNew chat: /new"
   NEW_CHAT = "You are in new chat.\n"
+  ALERT_MESSAGE = "You don't need to use /ai command."
 
   def call
     Telegram::Bot::Client.run(TOKEN) do |bot|
@@ -23,24 +24,21 @@ class ChatBot
                                  reply_to_message_id: message.message_id,
                                  text: HELP)
           elsif message.text[0..2] == '/ai'
-            query = message.text[3..-1]
-            result = MessageCreate.call(telegram_chat_id: message.chat.id,
-                                        telegram_user_id: message.from.id,
-                                        message: query).chat
-            Rails.logger.info("Message from result: #{result.message}")
-            answer = Chat.call(message: result.message).result
-            MessageCreate.call(telegram_chat_id: message.chat.id,
-                                        telegram_user_id: message.from.id,
-                                        message: answer).chat
+            ai_answer = response_create(message)
+            bot.api.send_message(chat_id: message.chat.id,
+                                 text: ALERT_MESSAGE)
             bot.api.send_message(chat_id: message.chat.id,
                                  reply_to_message_id: message.message_id,
-                                 text: answer)
+                                 text: ai_answer)
           elsif message.text[0..3] == '/new'
             Rails.logger.info("message.chat.id: #{message.chat.id}, message.from.id: #{message.from.id}")
             ChatMessage.where(telegram_chat_id: message.chat.id, telegram_user_id: message.from.id).delete_all
             bot.api.send_message(chat_id: message.chat.id, text: "#{NEW_CHAT}#{PARAM_ERROR}")
           else
-            bot.api.send_message(chat_id: message.chat.id, text: PARAM_ERROR)
+            ai_answer = response_create(message)
+            bot.api.send_message(chat_id: message.chat.id,
+                                 reply_to_message_id: message.message_id,
+                                 text: ai_answer)
           end
         rescue StandardError => e
           Rails.logger.info(e.message)
@@ -48,5 +46,19 @@ class ChatBot
         end
       end
     end
+  end
+  
+  private
+  def response_create(message_obj)
+    query = message_obj.text[3..-1]
+    result = MessageCreate.call(telegram_chat_id: message_obj.chat.id,
+                                telegram_user_id: message_obj.from.id,
+                                message: query).chat
+    Rails.logger.info("Message from result: #{result.message}")
+    answer = Chat.call(message: result.message).result
+    MessageCreate.call(telegram_chat_id: message_obj.chat.id,
+                                telegram_user_id: message_obj.from.id,
+                                message: answer).chat
+    answer
   end
 end
